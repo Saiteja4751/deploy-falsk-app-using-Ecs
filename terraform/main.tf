5357879db7db4b8ca72d909550248e0c477ba51b
Saiteja4751 <sai.battu@atyeti.com>
2025-07-04 12:52:05 +0530

    llll

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,57 @@
+# Get the existing ECR repo
 data "aws_ecr_repository" "app_repo" {
   name = var.app_name
 }
 
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get existing security group by name (instead of creating it)
+data "aws_security_group" "existing_sg" {
+  filter {
+    name   = "group-name"
+    values = ["${var.app_name}-sg"]
+  }
+
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# IAM role (create if needed)
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "${var.app_name}-ecs-task-execution"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
       }
-    ]
+    }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+# ECS cluster (create if not found)
 resource "aws_ecs_cluster" "app_cluster" {
   name = "${var.app_name}-cluster"
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 resource "aws_ecs_task_definition" "app_task" {
@@ -34,30 +61,18 @@ resource "aws_ecs_task_definition" "app_task" {
   cpu                     = "256"
   memory                  = "512"
   execution_role_arn      = aws_iam_role.ecs_task_execution_role.arn
-  container_definitions   = jsonencode([
+
+  container_definitions = jsonencode([
     {
       name      = var.app_name
       image     = "${data.aws_ecr_repository.app_repo.repository_url}:latest"
       essential = true
-      portMappings = [
-        {
-          containerPort = 5000
-          hostPort      = 5000
-        }
-      ]
+      portMappings = [{
+        containerPort = 5000
+        hostPort      = 5000
+      }]
     }
   ])
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
 }
 
 resource "aws_ecs_service" "app_service" {
@@ -68,28 +83,8 @@ resource "aws_ecs_service" "app_service" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = data.aws_subnets.default.ids
+    subnets          = data.aws_subnets.default.ids
     assign_public_ip = true
-    security_groups  = [aws_security_group.allow_all.id]
-  }
-}
-
-resource "aws_security_group" "allow_all" {
-  name        = "${var.app_name}-sg"
-  description = "Allow all inbound traffic"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups  = [data.aws_security_group.existing_sg.id]
   }
 }
