80847d06298d94caf71e98ede8fb8fc15e0137c7
Saiteja4751 <sai.battu@atyeti.com>
2025-07-04 14:18:05 +0530

    adddddddd

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from ECS Flask App sai!"
+    return "Hello from ECS Flask App sai and co!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
