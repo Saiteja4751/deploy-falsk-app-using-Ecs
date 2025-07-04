2ea5ea759e29ce0c49d1cd609c3d52adbd8e8462
Saiteja4751 <sai.battu@atyeti.com>
2025-07-04 10:43:51 +0530

    one to another

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from ECS Flask App with the help of the ECR!"
+    return "Hello from ECS Flask App with the help of the ECR for me!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
