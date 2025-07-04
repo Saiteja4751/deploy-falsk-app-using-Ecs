0948416a322213d168eb992c5d7aa1b6d38d9609
Saiteja4751 <sai.battu@atyeti.com>
2025-07-04 10:23:09 +0530

    add my application

---
+++ terraform/main.tf
@@ -0,0 +1,73 @@
+resource "aws_ecr_repository" "app_repo" {
+  name = var.app_name
+}
+
+resource "aws_ecs_cluster" "cluster" {
+  name = "${var.app_name}-cluster"
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.app_name}-ecs-task-execution"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_attach" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_task_definition" "app_task" {
+  family                   = var.app_name
+  requires_compatibilities = ["FARGATE"]
+  network_mode            = "awsvpc"
+  cpu                     = "256"
+  memory                  = "512"
+  execution_role_arn      = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([{
+    name      = var.app_name
+    image     = "${aws_ecr_repository.app_repo.repository_url}:latest"
+    essential = true
+    portMappings = [{
+      containerPort = 5000
+      hostPort      = 5000
+    }]
+  }])
+}
+
+resource "aws_ecs_service" "app_service" {
+  name            = "${var.app_name}-service"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.app_task.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets          = [data.aws_subnet_ids.default.ids[0]]
+    assign_public_ip = true
+    security_groups  = [data.aws_security_group.default.id]
+  }
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = data.aws_vpc.default.id
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,3 @@
+variable "app_name" {
+  default = "flask-ecs-app"
+}
