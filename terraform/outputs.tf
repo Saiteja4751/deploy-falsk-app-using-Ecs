5dc68557ebfa69a45c1a94818169d1dd0439502b
Saiteja4751 <sai.battu@atyeti.com>
2025-07-07 10:02:35 +0530

    addd

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,4 @@
-output "ecr_repo_url" {
-  value = aws_ecr_repository.flask_repo.repository_url
-}
-
-output "cluster_name" {
-  value = aws_ecs_cluster.flask_cluster.name
-}
-
-output "task_family" {
-  value = aws_ecs_task_definition.flask_task.family
+output "ecr_repository_url" {
+  description = "The URL of the created ECR repository"
+  value       = aws_ecr_repository.flask_repo.repository_url
 }
