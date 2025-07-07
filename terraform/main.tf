3f49449a59315217a261bd0c62f700b699fa2489
Saiteja4751 <sai.battu@atyeti.com>
2025-07-07 10:59:13 +0530

    dsd

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,61 @@
 resource "aws_ecr_repository" "flask_repo" {
   name = var.ecr_repo_name
 }
+
+
+resource "aws_ecs_cluster" "flask_cluster" {
+  name = var.ecs_cluster_name
+}
+
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "ecs_task_execution_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_attach" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_task_definition" "flask_task" {
+  family                   = "flask-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+  container_definitions    = jsonencode([{
+    name  = "flask-container"
+    image = var.ecr_image_uri
+    portMappings = [{
+      containerPort = 5000
+      protocol      = "tcp"
+    }]
+  }])
+}
+
+resource "aws_ecs_service" "flask_service" {
+  name            = "flask-service"
+  cluster         = aws_ecs_cluster.flask_cluster.id
+  task_definition = aws_ecs_task_definition.flask_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = var.subnet_ids
+    security_groups  = [var.security_group_id]
+    assign_public_ip = true
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.ecs_task_execution_attach]
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,22 @@ variable "ecr_repo_name" {
   description = "ECR Repository Name"
   type        = string
 }
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "ecs_cluster_name" {
+  default = "flask-cluster"
+}
+
+variable "ecr_image_uri" {
+  type = string
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "security_group_id" {
+  type = string
+}
